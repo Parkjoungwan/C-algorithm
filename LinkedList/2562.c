75dada68ff2fa5989aa89a061d6f46c6a372631b
parkjoungwan <whddhs6645@gmail.com>
2020-09-01 08:56:26 +0900

    Update LinkedList/2562.c, 2562 : 2562 solved

--- LinkedList/2562.c
+++ LinkedList/2562.c
@@ -1,14 +1,52 @@
 #include <stdio.h>
-int N;
+#include <stdlib.h>
+
+struct Linked_list{
+	struct Linked_list *next;
+	int data;	
+};
 int order;
 int max;
-struct Linked_list{
-	
+struct Linked_list *head;
+struct Linked_list *last;
+void insert(int i){
+	int N;
+	scanf("%d",&N);
+	struct Linked_list *node = malloc(sizeof(struct Linked_list));
+	if(i==0){
+		node->next=NULL;
+		node->data=N;
+		head=node;
+		last=node;
+	}else{
+		node->next=NULL;
+		node->data=N;
+		last->next=node;
+		last=node;
+	}
 }
-void nojam2562(){
+void search(){
+	struct Linked_list *curr=head;
+	max=curr->data;
+	int i=1;
+	while(curr != NULL){
+		if(curr->data>max){
+			max=curr->data;
+			order=i;
+		}
+		curr=curr->next;
+	       i++;	
+	}
+	printf("%d\n%d",max,order);
+}
 
+void nojam2562(){
+	for (int i=0;i<9;i++){
+		insert(i);
+	}
+	search();
 }
 
 int main(){
-	
+	nojam2562();
 }
