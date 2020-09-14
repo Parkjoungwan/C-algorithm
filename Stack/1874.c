28798f23e211599642f20d736384ffd07c3e537b
parkjoungwan <whddhs6645@gmail.com>
2020-09-14 22:02:47 +0900

    Update. Stack/1874 1874.c : solving

--- Stack/1874.c
+++ Stack/1874.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-struct Node{
+typedef struct Node{
 	struct Node * pre;
 	int data;
 }Node;
@@ -17,11 +17,28 @@ void nojam1874(){
 	for(int i=1;i<=N;i++){
 		scanf("%d",&num[i]);
 	}
-	for(int i=1;i<=N;i++){
-		for(int j=1;num[i]!=j;i++){
-			s_push(j);
+	int target=1;
+	int pushnum=1;
+	for(int i=1;i<=2*N;i++){
+		if(last==NULL){
+			s_push(pushnum);
+			printf("pushed: %d\n",pushnum);
+			pushnum++;
+		}else if(num[target]==last->data){
+			s_pop();
+			target++;
+			printf("poped: %d",num[target]);
+		}else{
+			s_push(pushnum);
+			printf("pushed: %d\n",pushnum);
+			pushnum++;
+		}
+		if(num[target]>last->data){
+			printf("NO");
+			return;
 		}
 	}
+	printf("YES");
 }
 
 int main(){
@@ -34,7 +51,7 @@ void s_push(int N){
 		node = malloc(sizeof(Node));
 		node->data = N;
 		last=node;
-		node->pre == NULL;
+		node->pre = NULL;
 	}else{
 		Node *node;
 		node = malloc(sizeof(Node));
@@ -42,12 +59,15 @@ void s_push(int N){
 		node->pre = last;
 		last= node;
 	}
-	printf("+\n");
 }
 void s_pop(){
 	Node *trash;
 	trash=last;
-	last=last->pre;
-	free(trash);
-	printf("-\n");
+	if(trash->pre==NULL){
+		last=NULL;
+		free(trash);
+	}else{
+		last=last->pre;
+		free(trash);
+	}
 }
