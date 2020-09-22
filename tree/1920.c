3fcf00ac093b73055ea3e498d8d7596a2033fe44
parkjoungwan <whddhs6645@gmail.com>
2020-09-22 17:47:57 +0900

    Update tree/1920.c : still solveing

--- tree/1920.c
+++ tree/1920.c
@@ -32,8 +32,50 @@ int mani(){
 }
 
 void t_push(int num){
+	Node *node;
+	node = malloc(sizeof(Node));
+	if(root==NULL){
+		root=node;
+		node->data=num;
+		node->Lnode=NULL;
+		node->Rnode=NULL;
+		return;
+	}
+	Node *tracker=root;
+	while(tracker!=NULL){
+		if(tracker->data<num){
+			if(tracker->Rnode==NULL){
+				tracker->Rnode=node;
+				node->data=num;
+				node->Lnode=NULL;
+				node->Rnode=NULL;
+			}
+			tracker=tracker->Rnode;
+		}else{
+			if(tracker->Lnode==NULL){
+				tracker->Lnode=node;
+				node->data=num;
+				node->Lnode=NULL;
+				node->Rnode=NULL;
+			}
+			tracekr=tracekr->Lnode;
+		}
+	}
 	return;
 }
-void t_find(int num){
-	return;
+void  t_find(int num){
+	Node *tracker=root;
+	int exist=0;
+	while(tracker!=NULL){
+		if(tracker->data<num){
+			tracker=tracker->Rnode;
+		}
+		if(tracekr->data>num){
+			tracekr=tracekr->Lnode;
+		}
+		if(tracker->data==num){
+			exist=1;
+		}
+	}
+	printf("%d",exist);
 }
