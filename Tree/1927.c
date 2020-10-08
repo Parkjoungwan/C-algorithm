63c53ff789485d5929d7a2bf336faa757ca156b4
parkjoungwan <whddhs6645@gmail.com>
2020-10-08 20:22:58 +0900

    1927 still not solve

---
+++ Tree/1927.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <limits.h>
+
+int N;
+int size;
+int Heap[100001];
+
+void minHeapPush();
+void minHeapPop();
+void swap();
+
+void nojam1927(){
+	scanf("%d",&N);
+	for(int i=0;i<N;i++){
+		int data;
+		scanf("%d",&data);
+		
+	}
+}
+
+int main(){
+	nojam1927();
+}
