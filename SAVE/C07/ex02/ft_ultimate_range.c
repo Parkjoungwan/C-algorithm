e00e923e2a24bce2cb90b735c25d59969b704664
parkjoungwan <whddhs6645@gmail.com>
2020-11-01 22:11:24 +0900

    Save ex02

--- SAVE/C07/ex02/ft_ultimate_range.c
+++ SAVE/C07/ex02/ft_ultimate_range.c
@@ -6,11 +6,32 @@
 /*   By: parkjoungwan <whddhs6645@gmail.com>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/01 20:18:07 by parkjoung         #+#    #+#             */
-/*   Updated: 2020/11/01 20:53:47 by parkjoung        ###   ########.fr       */
+/*   Updated: 2020/11/01 22:10:37 by parkjoung        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int ft_ultimate_range(int **range, int min, int max)
+#include <stdlib.h>
+
+int	ft_ultimate_range(int **range, int min, int max)
 {
-	int
-}	
+	int *result;
+	int size;
+	int index;
+
+	index = 0;
+	size = max - min;
+	if (max < min)
+	{
+		*range = 0;
+		return (0);
+	}
+	if (!(result = (int *)malloc(sizeof(int) * size)))
+	{
+		*range = 0;
+		return (-1);
+	}
+	while (index < size)
+		result[index++] = min++;
+	*range = result;
+	return (size);
+}
