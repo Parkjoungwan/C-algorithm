d825f2b9e0cdc176bc6d772fe296b0f1101b068a
parkjoungwan <whddhs6645@gmail.com>
2020-11-04 01:24:01 +0900

    save

--- SAVE/BSQ/ft_read.c
+++ SAVE/BSQ/ft_read.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_read.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: parkjoungwan <whddhs6645@gmail.com>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/11/04 00:09:02 by parkjoung         #+#    #+#             */
+/*   Updated: 2020/11/04 01:15:46 by parkjoung        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <fcntl.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -10,27 +22,25 @@ char	g_block_cha;
 char	g_box_cha;
 
 char	**ft_split(char *str, char *charset);
-
 int		ft_atoi(char *str);
 
-char	*ft_open_file(char *file_name)
+void	test_print(char **result)
 {
-	int		fd;
-	int		len;
-	int		index;
-	char	*one;
-	char	**result;
-	char	a;
-	
+	int index;
+
 	index = 0;
-	len = 0;
-	fd = open(file_name, O_RDONLY);
-	while(read(fd, &a, 1))
-		len++;
-	one = (char *)malloc(sizeof(char) * len);
-	read(fd, one, len);
-	close(fd);
-	while (one[index++] != '\n');
+	while(index < g_row_len)
+		printf("%s\n", result[index]);
+}
+
+char	**ft_send_2dm(char *one, int len)
+{
+	int index;
+	char	**result;
+
+	index = 0;
+	while (one[index] != '\n')
+		index++;
 	g_box_cha = one[index - 2];
 	g_block_cha = one[index - 3];
 	g_empty_cha = one[index - 4];
@@ -39,4 +49,26 @@ char	*ft_open_file(char *file_name)
 	g_row_len = ft_atoi(one);
 	g_col_len = (len - index) / g_row_len;
 	return (result);
+}
+
+void	ft_open_file(char *file_name)
+{
+	int		fd;
+	int		len;
+	char	a;
+	char	*one;
+
+	len = 0;
+	fd = open(file_name, O_RDONLY);
+	while (read(fd, &a, 1))
+		len++;
+	close(fd);
+	fd = open(file_name, O_RDONLY);
+	one = (char *)malloc(sizeof(char) * len);
+	read(fd, &one, len);
+	printf("%d\n", len);
+	printf("%d\n", one[len+1]);
+	printf("%d\n", a);
+	close(fd);
+	//ft_send_2dm(one, len);
 }
--- SAVE/BSQ/main.c
+++ SAVE/BSQ/main.c
@@ -1,8 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: parkjoungwan <whddhs6645@gmail.com>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/11/04 00:17:20 by parkjoung         #+#    #+#             */
+/*   Updated: 2020/11/04 00:18:23 by parkjoung        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <stdio.h>
 
-char *ft_open_file(char *file_name);
+char	*ft_open_file(char *file_name);
 
-int main(int argc, char **argv)
+int		main(int argc, char **argv)
 {
 	if (argc == 2)
 		ft_open_file(argv[1]);
